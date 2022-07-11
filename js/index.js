a9b46fef9d40a369a9a1156bcca18f4a1797e654
RenrevZ <harvy275@gmail.com>
2022-07-11 17:43:02 +0800

    final commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+//===== login page
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+if(signUpButton){
+	signUpButton.addEventListener('click', () => {
+		container.classList.add("right-panel-active");
+	});
+}
+
+if(signInButton){
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
+}
